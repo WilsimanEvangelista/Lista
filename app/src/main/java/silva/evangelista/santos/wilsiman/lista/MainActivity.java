6d499750c210f8ecc92361b33d270f6b2ba29877
20211TIIMI0281 <wilsiman.evangelista.ifes@gmail.com>
2023-04-12 07:58:29 -0300

    Initial commit

---
+++ app/src/main/java/silva/evangelista/santos/wilsiman/lista/MainActivity.java
@@ -0,0 +1,14 @@
+package silva.evangelista.santos.wilsiman.lista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
